9c995bedd595416eed22eca028373cb1f2ff290f
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-07-03 23:55:25 +0000

    x

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic"; // wymusza SSR przy każdym żądaniu
+export const dynamic = "force-dynamic";
 
 export default async function TVPage() {
   const res = await fetch(
@@ -8,17 +8,20 @@ export default async function TVPage() {
     {
       headers: {
         Accept: "application/vnd.github+json",
+        Authorization: process.env.GITHUB_TOKEN
+          ? `Bearer ${process.env.GITHUB_TOKEN}`
+          : undefined,
       },
     }
   );
 
   if (!res.ok) {
+    console.error("Response status:", res.status);
     throw new Error("Nie udało się pobrać najnowszego release z GitHub.");
   }
 
   const data = await res.json();
-
-  const versionTag = data.tag_name; // np. "1.9"
+  const versionTag = data.tag_name;
   const apkUrl = `https://github.com/FlowFlix/FlowFlix_Early_Alpha/releases/download/${versionTag}/app-release.apk`;
 
   redirect(apkUrl);
