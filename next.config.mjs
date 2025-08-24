2bb4bf2e0b72ee9b45b9b68d39268a4dc9dfa614
Julian <99652819+qJulianx@users.noreply.github.com>
2025-08-24 13:43:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://flowflix.pl",
+        permanent: true,
+      },
+    ]
   },
 }
 
