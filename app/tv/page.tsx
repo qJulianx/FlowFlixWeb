9e04dc70ab2de85c76daf9099fcac7acf525fe3e
Julian <99652819+qJulianx@users.noreply.github.com>
2025-09-02 22:32:27 +0200

    Update page.tsx

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 const GITHUB_REPO_OWNER = "FlowFlix";
 const GITHUB_REPO_NAME = "FlowFlix_Early_Alpha";
-const FALLBACK_APK_URL = "https://github.com/FlowFlix/FlowFlix_Early_Alpha/releases/download/2.2/FlowFlix.apk";
+const FALLBACK_APK_URL = "https://github.com/FlowFlix/FlowFlix_Early_Alpha/releases/download/2.3/FlowFlix.apk";
 
 export const dynamic = "force-dynamic";
 
